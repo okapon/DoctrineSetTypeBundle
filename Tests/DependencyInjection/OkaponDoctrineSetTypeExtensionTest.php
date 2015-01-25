be804ce0d4ddb6d0a77b36afc05d42e3f2184139
Yuichi Okada <yuuichi177@gmail.com>
2015-01-26 00:14:10 +0900

    Add test to confirm parameters is loaded

--- Tests/DependencyInjection/OkaponDoctrineSetTypeExtensionTest.php
+++ Tests/DependencyInjection/OkaponDoctrineSetTypeExtensionTest.php
@@ -13,5 +13,7 @@ class OkaponDoctrineSetTypeExtensionTest extends \PHPUnit_Framework_TestCase
         $loader = new OkaponDoctrineSetTypeExtension();
         $loader->load(array(array()), $container);
         $this->assertTrue($container->hasDefinition('doctrine_set_type.set_type_guesser'), 'SetTypeGuesser is loaded');
+        $this->assertEquals('Okapon\DoctrineSetTypeBundle\DBAL\Types\AbstractSetType', $container->getParameter('doctrine_set_type.set_type.class_name'));
+        $this->assertEquals('Okapon\DoctrineSetTypeBundle\Form\Guess\SetTypeGuesser', $container->getParameter('doctrine_set_type.set_type_guesser.class'));
     }
 }
